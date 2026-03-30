ec35148bef3d067ecc3ff97c948f29c244d258da
AA <centralemergenciaslosbronces@gmail.com>
2026-03-30 17:52:41 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,6 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCyiJHm68MhXC_2BmeH-KbRMeA3CYTEvko",
   authDomain: "cad-emergencias-d793a.firebaseapp.com",
@@ -17,10 +11,8 @@ const firebaseConfig = {
   appId: "1:1051475306664:web:6f1d43e296077930367a9d"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const app = initializeApp(firebaseConfig);
-
+// 🔥 Exportamos lo que vas a usar
 export const auth = getAuth(app);
 export const db = getFirestore(app);
